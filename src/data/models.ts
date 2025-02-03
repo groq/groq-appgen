0d45a01b689d2f79f3855e697eba81c7e95aa88d
Rick Lamers <ricklamers@gmail.com>
2025-02-03 23:45:50 +0100

    add 3.3 specdec (#17)

--- src/data/models.ts
+++ src/data/models.ts
@@ -1,1 +1,1 @@
-export const MODEL_OPTIONS = ["llama-3.3-70b-versatile", "deepseek-r1-distill-llama-70b-specdec", "deepseek-r1-distill-llama-70b"];
+export const MODEL_OPTIONS = ["deepseek-r1-distill-llama-70b-specdec", "llama-3.3-70b-specdec", "llama-3.3-70b-versatile", "deepseek-r1-distill-llama-70b"];
